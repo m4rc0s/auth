7fe480031501beb869ff52f13295a1ac3bc51266
mraraujo <marquinhoaraujo@gmail.com>
2023-06-08 17:59:28 -0300

    feat: create user

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+#[derive(Debug)]
+struct User {
+    username: String,
+    password: String
+}
+
+fn create_user(username: String, password: String) -> User {
+    let user = User { username, password };
+    return user;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let username = String::from("test@gmail.com");
+    let password = String::from("123456");
+
+    let user = create_user(username, password);
+
+    println!("New user created: {}", user.username);
+}
+
+#[cfg(test)]
+mod unit_test {
+
+    use super::*;
+
+    #[test]
+    fn should_create_user() {
+        let username = String::from("test");
+        let password = String::from("test");
+
+        let user = create_user(username, password);
+
+        assert_eq!(user.username, String::from("test"));
+        assert_eq!(user.password, String::from("test"));
+    }
+
 }
