2147fd1ee8de16974f803fa3ba2228b9ec2500dd
mraraujo <marquinhoaraujo@gmail.com>
2023-06-09 00:12:59 -0300

    chore: setup persistency and web framework for rest api

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,37 @@
-#[derive(Debug)]
+use serde::{Deserialize, Serialize};
+use skytable::{Connection, SkyResult};
+use skytable::ddl::{Ddl, Keymap, KeymapType};
+use skytable::actions::Actions;
+use serde_json;
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
 struct User {
     username: String,
-    password: String
+    password: String,
+    verified: bool
 }
 
 fn create_user(username: String, password: String) -> User {
-    let user = User { username, password };
+    let user = User { 
+        username: username, 
+        password: password, 
+        verified: true
+    };
+    
     return user;
 }
 
-fn main() {
-    let username = String::from("test@gmail.com");
+fn main() -> SkyResult<()> {
+    let mut con = Connection::new("127.0.0.1", 2003)?;
+
+    let username = String::from("test6@gmail.com");
     let password = String::from("123456");
 
     let user = create_user(username, password);
 
-    println!("New user created: {}", user.username);
+    con.set(&user.username, serde_json::to_string(&user).unwrap()).unwrap();
+
+    Ok(())
 }
 
 #[cfg(test)]
@@ -32,6 +48,17 @@ mod unit_test {
 
         assert_eq!(user.username, String::from("test"));
         assert_eq!(user.password, String::from("test"));
+    }
+
+    #[test]
+    fn should_convert_to_json() {
+        let username = String::from("test");
+        let password = String::from("test");
+
+        let user = create_user(username, password);
+
+        serde_json::to_string(&user).unwrap();
+
     }
 
 }
